ba67617ec41d1b527988e9ccffcbe3030753ed5a
Arthur Silva Sens <arthursens2005@gmail.com>
2025-03-30 18:18:21 +0100

    Add comment explaining replacement of multiple underscores
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- metric_name_builder.go
+++ metric_name_builder.go
@@ -97,6 +97,8 @@ func BuildCompliantMetricName(metric pmetric.Metric, namespace string, addMetric
 	}
 
 	// Simple case (no full normalization, no units, etc.).
+	// Note that this has the side effect of replacing multiple consecutive underscores with a single underscore.
+	// This is part of the OTel to Prometheus specification: https://github.com/open-telemetry/opentelemetry-specification/blob/v1.38.0/specification/compatibility/prometheus_and_openmetrics.md#otlp-metric-points-to-prometheus.
 	metricName := strings.Join(strings.FieldsFunc(metric.Name(), func(r rune) bool {
 		return invalidMetricCharRE.MatchString(string(r))
 	}), "_")
