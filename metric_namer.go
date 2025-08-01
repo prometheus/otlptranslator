10a0f69acb3a5e6c588c1cd04e7a106b103a42e2
Owen Williams <owen.williams@grafana.com>
2025-08-01 10:53:39 -0400

    Remove much too slow regex (#50)
    Signed-off-by: Owen Williams <owen.williams@grafana.com>

--- metric_namer.go
+++ metric_namer.go
@@ -186,13 +186,13 @@ func (mn *MetricNamer) buildCompliantMetricName(name, unit string, metricType Me
 
 	// Simple case (no full normalization, no units, etc.).
 	metricName := strings.Join(strings.FieldsFunc(name, func(r rune) bool {
-		return invalidMetricCharRE.MatchString(string(r))
+		return !isValidCompliantMetricChar(r) && r != '_'
 	}), "_")
 
 	// Namespace?
 	if mn.Namespace != "" {
 		namespace := strings.Join(strings.FieldsFunc(mn.Namespace, func(r rune) bool {
-			return invalidMetricCharRE.MatchString(string(r))
+			return !isValidCompliantMetricChar(r) && r != '_'
 		}), "_")
 		normalizedName = namespace + "_" + metricName
 		return
@@ -207,11 +207,7 @@ func (mn *MetricNamer) buildCompliantMetricName(name, unit string, metricType Me
 	return
 }
 
-var (
-	// Regexp for metric name characters that should be replaced with _.
-	invalidMetricCharRE   = regexp.MustCompile(`[^a-zA-Z0-9:_]`)
-	multipleUnderscoresRE = regexp.MustCompile(`__+`)
-)
+var multipleUnderscoresRE = regexp.MustCompile(`__+`)
 
 // isValidCompliantMetricChar checks if a rune is a valid metric name character (a-z, A-Z, 0-9, :).
 func isValidCompliantMetricChar(r rune) bool {
